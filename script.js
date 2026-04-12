e76427c86ebbb3c7d919454d450b9614d08e265e
valentinebot887 <crisluci630@gmail.com>
2026-04-13 00:16:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-let currentPage = 1;
+let currentPage = 0;
+const pages = document.querySelectorAll(".page");
 
 function nextPage() {
-  document.getElementById("page" + currentPage).classList.remove("active");
+  pages[currentPage].classList.remove("active");
   currentPage++;
-  document.getElementById("page" + currentPage).classList.add("active");
+  pages[currentPage].classList.add("active");
+}
+
+// Countdown
+const weddingDate = new Date("Dec 6, 2026 00:00:00").getTime();
+
+setInterval(() => {
+  const now = new Date().getTime();
+  const diff = weddingDate - now;
+
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+
+  document.getElementById("countdown").innerHTML =
+    "⏳ " + days + " Days Remaining";
+}, 1000);
+
+// Music
+const music = document.getElementById("bgMusic");
+
+function toggleMusic() {
+  if (music.paused) {
+    music.play();
+  } else {
+    music.pause();
+  }
 }
