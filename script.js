c0f6c76ff8a220640253da8e55d894c0cc346e19
valentinebot887 <crisluci630@gmail.com>
2026-04-13 20:33:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,43 @@
 const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzk73ZML3j_k-kaJ_omZooIqObQXgqSi4XhTQh2_1mt42h4H9GLIVevw_3jwa401PD7yw/exec";
 
-let currentPage = 0;
 let pages;
+let currentPage = 0;
 
 document.addEventListener("DOMContentLoaded", () => {
-
   pages = document.querySelectorAll(".page");
-
-  document.querySelector(".envelope-body").onclick = () => {
-    document.querySelector(".envelope-body").classList.add("envelope-open");
-
-    setTimeout(() => {
-      document.getElementById("envelope").style.display = "none";
-      document.getElementById("mainContent").style.display = "block";
-    }, 1000);
-  };
-
 });
 
 /* NAVIGATION */
+function showPage(index) {
+  pages.forEach(p => p.classList.remove("active"));
+  pages[index].classList.add("active");
+}
+
 function nextPage() {
   if (currentPage < pages.length - 1) {
-    pages[currentPage].classList.remove("active");
     currentPage++;
-    pages[currentPage].classList.add("active");
+    showPage(currentPage);
   }
 }
 
 function prevPage() {
   if (currentPage > 0) {
-    pages[currentPage].classList.remove("active");
     currentPage--;
-    pages[currentPage].classList.add("active");
+    showPage(currentPage);
   }
 }
 
 /* RSVP */
 function submitRSVP() {
+  let name = document.getElementById("name").value;
+
+  if (!name) {
+    alert("Enter your name");
+    return;
+  }
+
   let formData = new FormData();
-  formData.append("name", document.getElementById("name").value);
-  formData.append("guests", document.getElementById("guests").value);
-  formData.append("status", document.getElementById("status").value);
+  formData.append("name", name);
 
   fetch(SCRIPT_URL, {
     method: "POST",
@@ -48,15 +45,5 @@ function submitRSVP() {
     body: formData
   });
 
-  alert("✅ Submitted!");
-}
-
-/* ADMIN */
-function checkAdmin() {
-  if (document.getElementById("pin").value === "06122026") {
-    document.getElementById("loginBox").style.display = "none";
-    document.getElementById("adminPanel").style.display = "block";
-  } else {
-    alert("Wrong PIN");
-  }
+  alert("Submitted ✅");
 }
